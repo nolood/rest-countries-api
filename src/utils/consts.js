b62ea99e6f29362c63186b0ae45d4224fe3625fa
nolood <124226446+nolood@users.noreply.github.com>
2023-03-05 17:28:37 +0500

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,3 @@
 export const HOME_ROUTE = '/rest-countries-api'
-export const COUNTRY_ROUTE = '/country'
+export const COUNTRY_ROUTE = '/rest-countries-api/country'
 export const PUBLIC_URL = 'https://nolood.github.io/rest-countries-api/'
